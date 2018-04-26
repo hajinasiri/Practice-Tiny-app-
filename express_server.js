32407d6ff42429a139d98f6ff8998fba2c0ca050
vagrant <hajinasiri@gmail.com>
2018-04-26 02:04:58 +0000

    new page and post request handler from new page were added.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//This function generates a 6 character string
+function generateRandomString() {
+  var shortString = "";
+  var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  while (shortString.length < 6) {
+      shortString += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+  console.log(shortString);
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,14 +30,27 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
 
+//To show all the urls
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// To render the page for adding new urls
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//The raute to show a single url
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+//To handle post request from the "new" page to add to the database
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  generateRandomString();
+  res.send("Ok");
 });
